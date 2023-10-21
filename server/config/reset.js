4585ff209d1255322a7e2e28ffe9324e1d9ce2a9
Mumtaz <fatim25m@mtholyoke.edu>
2023-10-21 12:03:04 -0400

    ignore .env

--- server/config/reset.js
+++ server/config/reset.js
@@ -70,7 +70,7 @@ const seedTripsTable = async() => {
 const createDestinationsTable = async() => {
     const createDestinationsTableQuery = `
     CREATE TABLE IF NOT EXISTS destinations(
-        id PRIMARY serial KEY,
+        id serial PRIMARY KEY,
         destination varchar(100) NOT NULL,
         description varchar(500) NOT NULL,
         city varchar(100) NOT NULL,
@@ -87,4 +87,56 @@ const createDestinationsTable = async() => {
     }
 }
 
+const createActivitiesTable = async () => {
+    const createActivitiesTableQuery = `
+        CREATE TABLE IF NOT EXISTS activities (
+            id serial PRIMARY KEY,
+            trip_id int NOT NULL,
+            activity varchar(100) NOT NULL,
+            num_votes integer DEFAULT 0,
+            FOREIGN KEY(trip_id) REFERENCES trips(id)
+        );
+    `
+    try{
+        const res = await pool.query(createActivitiesTableQuery)
+        console.log('Destinations table created successfully');
+    } catch(error){
+        console.log('⚠️ error creating activities table', error);
+    }
+}
+
+const createTripsDestinationsTable = async () => {
+    const createTripsDestinationsTableQuery = `
+        CREATE TABLE IF NOT EXISTS trips_destinations (
+            trip_id int NOT NULL,
+            destination_id int NOT NULL,
+            PRIMARY KEY (trip_id, destination_id),
+            FOREIGN KEY (trip_id) REFERENCES trips(id) ON UPDATE CASCADE,
+            FOREIGN KEY (destination_id) REFERENCES destinations(id) ON UPDATE CASCADE
+        );
+    `
+    try {
+        const res = await pool.query(createTripsDestinationsTableQuery)
+        console.log('🎉 trips_destinations table created successfully')
+      } catch (err) {
+        console.error('⚠️ error creating trips_destinations table', err)
+      }
+  }
+
+  const createUsersTable = async () => {
+    const createUsersTableQuery = `
+        CREATE TABLE IF NOT EXISTS users(
+            id serial PRIMARY KEY,
+            githubid integer NOT NULL,
+            username varchar(100) NOT NULL,
+            avatarurl varchar(500) NOT NULL,
+            accesstoken varchar(500) NOT NULL
+        )`
+        
+  }
+
+
+
 seedTripsTable()
+createDestinationsTable()
+createActivitiesTable()
